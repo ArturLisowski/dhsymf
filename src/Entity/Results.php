b17c7f5afabe7d3914c3c00982cafb961124b58b
Artur Lisowski <artur@lisart.pl>
2019-09-28 22:21:41 +0200

    setters and getters for results model

--- src/Entity/Results.php
+++ src/Entity/Results.php
@@ -9,6 +9,13 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Results
 {
+
+    const GRADE5 = 'bardzo dobry';
+    const GRADE4 = 'dobry';
+    const GRADE3 = 'dostateczny';
+    const GRADE2 = 'dopuszczający';
+    const GRADE1 = 'niedostateczny';
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -34,8 +41,76 @@ class Results
      */
     private $comment;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setStudentId(int $value)
+    {
+        $this->studentId = $value;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getStudentId(): ?int
+    {
+        return $this->studentId;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setPoints(int $value)
+    {
+        $this->points = $value;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPoints(): ?int
+    {
+        return $this->points;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     * @throws \Exception
+     */
+    public function setGrade(string $value)
+    {
+        if (!in_array($value, [
+            Results::GRADE5,
+            Results::GRADE4,
+            Results::GRADE3,
+            Results::GRADE2,
+            Results::GRADE1,
+        ], true)) {
+            throw new \Exception('Result - setGrade incorect value :' . $value, 400);
+        }
+
+        $this->grade = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGrade(): string
+    {
+        return $this->grade;
     }
 }
